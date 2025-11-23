6a77f552f6edd40fbb5d0cc1d4b02f16d497b77b
aryandi-putra <aryandi2712@gmail.com>
2025-11-24 02:03:36 +0700

    add more test to repository

--- app/src/test/java/com/aryandi/news/data/repository/NewsRepositoryImplTest.kt
+++ app/src/test/java/com/aryandi/news/data/repository/NewsRepositoryImplTest.kt
@@ -2,6 +2,8 @@ package com.aryandi.news.data.repository
 
 import com.aryandi.data.model.Article
 import com.aryandi.data.model.NewsList
+import com.aryandi.data.model.Source
+import com.aryandi.data.model.SourcesList
 import com.aryandi.data.network.ApiResponse
 import com.aryandi.data.network.ApiService
 import com.aryandi.data.repository.NewsRepository
@@ -25,30 +27,72 @@ class NewsRepositoryImplTest {
     }
 
     @Test
-    fun testResponseSuccess() = runTest {
-        coEvery { apiService.getNewsBySource("abc-news", page = 1) } returns (getSuccessData())
+    fun testGetSourceResponseSuccess() = runTest {
+        coEvery { apiService.getSources("general") } returns (getSourcesSuccessData())
+        val response = repository.getSourceList("general")
+        assertEquals(ApiResponse.Success(getSourcesSuccessData().sources), response.last())
+    }
+
+    @Test
+    fun testGetNewsBySourceResponseSuccess() = runTest {
+        coEvery { apiService.getNewsBySource("abc-news", page = 1) } returns (getArticlesData())
         val response = repository.getNewsList("abc-news", currentPage = 1)
-
-        assertEquals(ApiResponse.Success(getSuccessData().articles), response.last())
+        assertEquals(ApiResponse.Success(getArticlesData().articles), response.last())
     }
 
     @Test
-    fun testEmptyResponseErrorMessage() = runTest {
-        coEvery { apiService.getNewsBySource(source = "abc-news", page = 1) } returns (getEmptyData())
+    fun testGetSourceResponseEmptyMessage() = runTest {
+        coEvery { apiService.getSources("general") } returns (getSourcesEmptyData())
+        val response = repository.getSourceList("general")
+        assertEquals(
+            ApiResponse.Error("No sources available now, please check after a while!"), response.last()
+        )
+    }
+
+    @Test
+    fun testGetNewsBySourceResponseEmptyMessage() = runTest {
+        coEvery {
+            apiService.getNewsBySource(
+                source = "abc-news",
+                page = 1
+            )
+        } returns (getNewsEmptyData())
         val response = repository.getNewsList(source = "abc-news", currentPage = 1)
-
-        assertEquals(ApiResponse.Error("No news available now, please check after a while!"), response.last())
+        assertEquals(
+            ApiResponse.Error("No news available now, please check after a while!"), response.last()
+        )
     }
 
     @Test
-    fun testNullResponseMessage() = runTest {
-        coEvery { apiService.getNewsBySource(source = "abc-news", page = 1) } throws (NullPointerException())
+    fun testGetSourceResponseErrorMessage() = runTest {
+        coEvery { apiService.getSources(category = "general") } throws (NullPointerException())
+        val response = repository.getSourceList(category = "general")
+        assertEquals(ApiResponse.Error("Unexpected Error"), response.last())
+    }
+
+    @Test
+    fun testGetNewsBySourceResponseErrorMessage() = runTest {
+        coEvery {
+            apiService.getNewsBySource(
+                source = "abc-news", page = 1
+            )
+        } throws (NullPointerException())
         val response = repository.getNewsList(source = "abc-news", currentPage = 1)
 
         assertEquals(ApiResponse.Error("Unexpected Error"), response.last())
     }
 
-    fun getSuccessData(): NewsList {
+    fun getSourcesSuccessData(): SourcesList {
+        return SourcesList(
+            "ok", sources = arrayListOf(
+                Source(
+                    id = "abc-news", name = "ABC News"
+                )
+            )
+        )
+    }
+
+    fun getArticlesData(): NewsList {
         return NewsList(
             "ok", totalResults = 1, articles = arrayListOf(
                 Article(
@@ -65,11 +109,15 @@ class NewsRepositoryImplTest {
         )
     }
 
-    fun getEmptyData(): NewsList {
+    fun getSourcesEmptyData(): SourcesList {
+        return SourcesList(
+            "ok", sources = arrayListOf()
+        )
+    }
+
+    fun getNewsEmptyData(): NewsList {
         return NewsList(
-            "ok",
-            totalResults = 0,
-            articles = arrayListOf()
+            "ok", totalResults = 0, articles = arrayListOf()
         )
     }
 }
